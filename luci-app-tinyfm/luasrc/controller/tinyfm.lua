c3e1482b6febb743d7acacb7a79ede8d04adba45
Maizil41 <maizilpc7@gmail.com>
2026-05-25 08:08:30 +0700

    update

--- luci-app-tinyfm/luasrc/controller/tinyfm.lua
+++
@@ -1,4 +0,0 @@
-module("luci.controller.tinyfm", package.seeall)
-function index()
-entry({"admin","system","tinyfm"}, template("tinyfm"), _("File Manager"), 45).leaf=true
-end
--- luci-app-tinyfm/root/etc/uci-defaults/tinyfm-symlink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /
-[ ! -d /www/tinyfm/rootfs ] && ln -s / /www/tinyfm/rootfs
-[ ! -d /www/tinyfm/openclash ] && ln -s /etc/openclash/ /www/tinyfm/openclash
-exit 0
